8330f05fee1599973780a47e432fb040d6c120e1
skitale-design <skitale85@gmail.com>
2024-07-03 18:04:47 +0300

    Добавить Bootstrap Table

--- angularapp20240630.client/src/app/emploees/emploees.component.ts
+++ angularapp20240630.client/src/app/emploees/emploees.component.ts
@@ -1,5 +1,10 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap-table';
+// import 'bootstrap-table-filter-control';
+
 
 interface Json{
 
@@ -24,18 +29,24 @@ interface Emploees {
   styleUrl: './emploees.component.css'
 })
 export class EmploeesComponent implements OnInit {
+
+  @ViewChild('table') table : ElementRef | undefined;
+  
   public emploees: Emploees[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private elementRef: ElementRef) { }
 
   ngOnInit() {
     this.getEmploees();
   }
 
   getEmploees() {
-    this.http.get<Json>('https://localhost:7242/emploees').subscribe(
+    this.http.get<Json>('https://localhost:4242/emploees').subscribe(
       (result) => {
         this.emploees = result.emloees;
+        if(this.table){
+          $(this.table.nativeElement).bootstrapTable({data: this.emploees})
+        }
         console.log(this.emploees)
       },
       (error) => {
