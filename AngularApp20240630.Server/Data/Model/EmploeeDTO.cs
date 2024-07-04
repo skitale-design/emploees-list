079ab3d8fa03ec9a34ab7fb4b4849a6836fdc204
Александр Ильин <alexvitkir@gmail.com>
2024-07-04 15:42:17 +0300

    Добавить CRUD

---
+++ AngularApp20240630.Server/Data/Model/EmploeeDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AngularApp20240630.Server.Data.Model
+{
+    internal class EmploeeDTO
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public string? MiddleName { get; set; }
+        public required string Department { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime HireDate { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
--- AngularApp20240630.Server/Program.cs
+++ AngularApp20240630.Server/Program.cs
@@ -1,4 +1,5 @@
 using AngularApp20240630.Server.Data;
+using AngularApp20240630.Server.Data.Model;
 using Microsoft.EntityFrameworkCore;
 
 namespace AngularApp20240630.Server
@@ -30,13 +31,63 @@ namespace AngularApp20240630.Server
             // Configure the HTTP request pipeline.
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
 
             app.MapGet("/emploees", async (EmploeeDbContext db) =>
             {
                 var emloees = await db.Emploees.ToListAsync();
-                return Results.Json(new { emloees });
+                return Results.Ok(emloees);
+
+            });
+
+            app.MapGet("/emploee/{id:int}", async (EmploeeDbContext db, int id) =>
+            {
+                var emploee = await db.Emploees.Where(x=>x.Id == id).ToListAsync();
+
+                if (emploee != null)
+                {
+                    return Results.Ok(emploee);
+                }
+                else
+                {
+                    return Results.NotFound();
+                }
+
+            });
+
+            app.MapPost("/create", async (EmploeeDbContext db, EmploeeDTO emploee) =>
+            {
+                Emploee newEmploee = new Emploee
+                {
+                    Id = 0,
+                    FirstName = emploee.FirstName,
+                    LastName = emploee.LastName,
+                    MiddleName = emploee.MiddleName,
+                    Department = emploee.Department,
+                    BirthDate = emploee.BirthDate,
+                    HireDate = emploee.HireDate,
+                    Salary = emploee.Salary
+                };
+
+                db.Add(newEmploee);
+                db.SaveChanges();
+
+                var emploees = await db.Emploees.ToListAsync();
+
+                return Results.Ok(emploees);
+
+            });
+
+            app.MapDelete("/{id:int}", async (EmploeeDbContext db, int id) =>
+            {
+                var emploee = await db.Emploees.Where(x => x.Id == id).FirstOrDefaultAsync();
+
+                db.Remove(emploee);
+                db.SaveChanges();
+
+                var emloees = await db.Emploees.ToListAsync();
+
+                return Results.Ok(emloees);
 
             });
 
